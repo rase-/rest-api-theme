7e7968aa7268228f3c3440875de3dc588f2f94c3
Tony Kovanen <tonykovanen@hotmail.com>
2015-10-13 21:21:58 +0300

    Add individual post

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ var PostsController = require('./lib/controllers/posts-controller');
 var postsController = new PostsController();
 
 page('/', postsController.postsPage.bind(postsController));
+page('/:postId', postsController.postPage.bind(postsController));
 page.start();
